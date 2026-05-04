f1b883f1561e966d69cc47bd8008c1c8119b5b77
ehimarejon123 <IhimareKing@gmail.com>
2026-05-04 19:37:55 +0100

    conditional statements, while loop and datatypes in javascript

---
+++ bootstrap/javascript/data.js
@@ -0,0 +1,10 @@
+let a = 20
+let b = 25.5
+let c = "true"
+let d = false
+let arr =[1,2,3]
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(arr)
---
+++ bootstrap/javascript/statements.js
@@ -0,0 +1,13 @@
+let marks = 70
+if(marks >=90){
+    console.log("grade A")
+}
+else if(marks >=60){
+    console.log("grade B")
+}
+else if(marks >=40){
+    console.log("grade C")
+}
+else {
+    console.log("grade F")
+}
