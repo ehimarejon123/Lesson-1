f8ebb0befb76ceebc7a26b0c52361990b9c41f9f
ehimarejon123 <IhimareKing@gmail.com>
2026-06-22 19:30:34 +0100

    my snack shop in python

---
+++ bootstrap/Python/mysnackshop.py
@@ -0,0 +1,26 @@
+snackname = "chips"
+price = 10
+weight = 2.0
+available = True
+
+quantity = 3
+totalcost = price * quantity
+affordable = price <= 50
+shopname = "Happy" + " snacks"
+firstletter = shopname[0]
+price1 = 20
+price2 = 50
+temp = price1
+price1 = price2
+price2 = temp
+
+print(snackname)
+print(weight)
+print(available)
+print(quantity)
+print(totalcost)
+print(affordable)
+print(shopname)
+print(firstletter)
+print(price1)
+print(price2)
